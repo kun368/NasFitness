008651e7574d3e26557b8a0fe496aa541218e0a9
kun368 <kun368@126.com>
2018-06-01 10:06:06 +0800

    支持四种使用方式！跨平台健身助手！

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import UserForm from "./components/UserForm/UserForm";
 import NebUtils from "../../util/NebUtils";
 import {Feedback} from '@icedesign/base';
 import RealTimeStatistics from "./components/RealTimeStatistics/RealTimeStatistics";
+import { Dialog } from '@icedesign/base';
 
 const Toast = Feedback.toast;
 
@@ -20,6 +21,7 @@ export default class Home extends Component {
       dataSource: [],
       prepared: false,
       userAddress: null,
+      welcomeDialogShow: true,
     };
   }
 
@@ -45,9 +47,38 @@ export default class Home extends Component {
     })
   }
 
+  onWelcomeDialogClose = () => {
+    this.setState({
+      welcomeDialogShow: false
+    });
+  };
+
+  renderWelcomeDialog() {
+    return (
+      <Dialog
+        visible={this.state.welcomeDialogShow}
+        onOk={this.onWelcomeDialogClose}
+        closable="esc,mask,close"
+        onCancel={this.onWelcomeDialogClose}
+        onClose={this.onWelcomeDialogClose}
+        title="欢迎体验新版星云健身助手！"
+      >
+        <p style={{color: 'red'}}>星云健身助手新版本隆重上线，可跨平台使用，率先支持四种使用方式，并添加详细的使用帮助，为您的身体健康保驾护航！</p>
+        <ul>
+          <li>方法一. Chrome浏览器打开本应用，安装WebExtensionWallet扩展，使用扩展本身交易</li>
+          <li>方法二. Chrome浏览器打开本应用，安装WebExtensionWallet扩展和NAS手机钱包，使用手机扫码交易</li>
+          <li>方法三. 手机/平板浏览器打开星云健身助手，上传文件时自动调用钱包交易（只需要安装NAS手机钱包）</li>
+          <li>方法四. 直接在NAS手机钱包DApp市场里选择本应用使用</li>
+        </ul>
+        <p style={{color: 'red'}}>新版本还添加了BMI分析等工具，更多详细使用方式请查看使用帮助手册！</p>
+      </Dialog>
+    );
+  }
+
   render() {
     return (
       <div className="home-page" style={{background: '#fff'}}>
+        {this.renderWelcomeDialog()}
         <Header/>
         <PlatformLanding/>
         <RealTimeStatistics
